2eae905ca6e0a8cfcd007a4c49735ad7e416f355
Sudarshan <raisingphoenix289@gmail.com>
2023-09-11 20:04:02 +0600

    Automated commit

---
+++ hash.sh
@@ -0,0 +1,12 @@
+
+# Step 3: Add all changes to the Git staging area
+git add .
+
+# Step 4: Commit the changes with a message
+git commit -m "Automated commit"
+
+# Step 5: Push the changes to the remote repository (change 'origin' and  
+'main' as needed)
+git push origin main
+
+
