71fdad137b477def3e91177c291199ab3e596a14
Sudarshan <raisingphoenix289@gmail.com>
2023-09-11 15:09:46 +0600

    Automated commit

--- enc.py
+++ enc.py
@@ -40,6 +40,8 @@ def encrypt_uu(json_data, key):
         item["uu"] = caesar_cipher(item["uu"], key)
         item["nn"] = caesar_cipher(item["nn"], key)
 
+    encrypted_json["tit1"] = caesar_cipher(item["tit1"], key)
+
 
     return encrypted_json
 
