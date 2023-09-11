4d92005ddbc687c31c6d2ad7d65ac583a3ba67a3
Sudarshan <raisingphoenix289@gmail.com>
2023-09-11 19:55:35 +0600

    Automated commit

--- enc.py
+++ enc.py
@@ -2,6 +2,7 @@ import json
 
 input_file_path = "ini.json" 
 output_file_path = "ini2.json"
+output_file_path_de = "de_ini2.json"
 
 try:
     with open(input_file_path, "r") as input_file:
@@ -62,6 +63,14 @@ try:
 except Exception as e:
     print(f"Error occurred while writing to the output JSON file: {str(e)}")
     exit(1)
+
+try:
+    with open(output_file_path_de, "w") as output_file:
+        json.dump(encrypted_json, output_file, indent=2)  # Indent for pretty formatting
+except Exception as e:
+    print(f"Error occurred while writing to the output JSON file: {str(e)}")
+    exit(1)
+
 
 print(f"Processed JSON data saved to '{output_file_path}' successfully.")
 
